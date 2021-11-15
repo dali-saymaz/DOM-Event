175de47400766e898abd6bb746bc69cc28aef149
unknown <dalidasy24@gmail.com>
2021-11-15 21:07:40 +0100

    first commit

---
+++ 1/script.js
@@ -0,0 +1,56 @@
+// What You Will Learn
+// // 1 How to change the content of HTML elements
+// let dali;
+
+// dali = document.getElementById('lorem1');
+// console.log(dali);
+// dali = document.getElementById('lorem1').id;
+// console.log(dali);
+// dali = document.getElementById('lorem1').className;
+// console.log(dali);
+
+// // dali = document.getElementById('lorem1');
+// // //dali = dali.id;
+// // dali = dali.className;
+// // console.log(dali);
+
+
+// // 2 How to change the style (CSS) of HTML elements
+// let dali;
+// dali = document.getElementById('lorem1');
+// dali.style.fontSize='60px';
+// //dali.style.color='red';
+// //dali.style.fontWeight='bold';
+// //dali.style.display='none';
+
+
+// 4 How to add and delete HTML elements
+// let dali;
+// document.getElementById('lorem1').innerText='ben bu isi ogrendim gibi';
+// document.getElementById('lorem2').innerText='<b>ben bu isi ogrendim gibi</b>';
+// document.getElementById('lorem3').innerHTML='<b>ben bu isi ogrendim gibi</b>';
+// console.log(dali);
+
+
+// 5 document.querySelector()//id classname ve taglara gore secimlerin hepsinde kullanilir
+// let dali;
+// console.log(document.querySelector('#lorem1'));
+// console.log(document.querySelector('.text'));//html sayfasindaki ilk classname text olani secer digerlerini secmez
+// console.log(document.querySelector('p'));//html sayfasindaki ilk p tagini  secer digerlerini secmez
+
+// document.querySelector('p').style.color='red';
+// document.querySelector('p:last-child').style.color='blue';
+// document.querySelector('p:nth-child(2)').style.color='yellow';
+// document.querySelector('p:nth-child(3)').textContent='textcontent ile degistirdim';
+// document.querySelector('p:nth-child(4)').textContent='textcontent ile degistirdim';
+// // document.querySelectorAll('p').style.color='red';//colu secim yapar butun pleri secer
+// //document.querySelectorAll('p').style.fontSize='40px';//colu secim yapar butun pleri secer
+
+
+// 6 className classList olayi
+
+// document.getElementById('lorem1').className='eklenenclassname2';
+// document.getElementById('lorem2').classList.add('eklenenclassname');
+// console.log(lorem2);
+
+
---
+++ 2/script.js
@@ -0,0 +1,14 @@
+
+//document.getElementsByTagName()
+let val;
+val = document.getElementsByTagName('li');
+console.log(val)
+val = document.getElementById('task-list');
+console.log(val)
+val = val.getElementsByTagName('a');
+console.log(val)
+
+
+// document.querySelectorAll()
+val = document.querySelectorAll('li');
+console.log(val)
